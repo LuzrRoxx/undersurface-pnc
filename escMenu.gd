08a313acbd42d924eb8caa0977d964433228ff50
LuzrRoxx <loserrroxx09@gmail.com>
2026-02-03 14:07:05 -0600

    pausing and such

--- escMenu.gd
+++ escMenu.gd
@@ -6,3 +6,7 @@ func _process(delta):
 	if Input.is_action_just_pressed("game_pause"):
 		print("pause")
 		controlNode.visible = !visible
+	if controlNode.visible:
+		get_tree().paused = true
+	else:
+		get_tree().paused = false
